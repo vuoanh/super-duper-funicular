2bbcfe7b4e54c5ae63da5ef6631363ba7cdd5aaf
Oanh Vu <vuoanh@grinnell.edu>
2017-07-25 15:53:49 -0400

    add travis test file

--- tests/test_math.py
+++ tests/test_math.py
@@ -9,5 +9,10 @@ def test_add():
     assert fcm.math.add(5, 2) == 7
     assert fcm.math.mult(2, 5) == 10
 
-def test_mult():
-    assert fcm.math.mult(2, 3) == 6
+testdata = [
+    (2, 5, 10),
+    (1, 2, 2)
+    ]
+@pytest.mark.parametrize("a, b, expected", testdata)
+def test_mult(a, b, expected):
+    assert fcm.math.mult(a, b) == expected
